7ad17657188e06139b3b57b758e5c37e0055e2a0
Aryan Singh <96579866+AryanSinghIIIT@users.noreply.github.com>
2023-01-02 19:07:09 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// REACT_APP_GOOGLE_MAPS_API_KEY=AIzaSyDAcZLBcKPjQ00Fr70MM_5U0tM4c9NAfxw
+// REACT_APP_RAPID_API_WEATHER_API_KEY=2e73cdf05dmsh832e0b111c49186p149b12jsn978f43c2bdad
